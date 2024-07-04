d3c15ea261220221a974abe4d0d572bf7a9cd03d
Eric Rabinowitz <ericrabinowitz@me.com>
2024-07-04 11:11:01 -0400

    prevent impossible polygons

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ export interface DomMapperOptions {
 }
 
 export type Point = [number, number];
+export type Triangle = [...Point, ...Point, ...Point];
 export type Box = [...Point, ...Point, ...Point, ...Point];
 
 interface Item {
@@ -113,10 +114,44 @@ function dommapper(element: HTMLElement, options: DomMapperOptions = {}) {
   update3dTransform(element, points);
 }
 
+function isPointInTriangle(point: Point, triangle: Triangle) {
+  const [x1, y1, x2, y2, x3, y3] = triangle;
+
+  // thank you mattdesl https://github.com/mattdesl/point-in-triangle
+  const cx = point[0],
+    cy = point[1],
+    v0x = x3 - x1,
+    v0y = y3 - y1,
+    v1x = x2 - x1,
+    v1y = y2 - y1,
+    v2x = cx - x1,
+    v2y = cy - y1,
+    dot00 = v0x * v0x + v0y * v0y,
+    dot01 = v0x * v1x + v0y * v1y,
+    dot02 = v0x * v2x + v0y * v2y,
+    dot11 = v1x * v1x + v1y * v1y,
+    dot12 = v1x * v2x + v1y * v2y;
+
+  // Compute barycentric coordinates
+  const b = dot00 * dot11 - dot01 * dot01,
+    inv = b === 0 ? 0 : 1 / b,
+    u = (dot11 * dot02 - dot01 * dot12) * inv,
+    v = (dot00 * dot12 - dot01 * dot02) * inv;
+  return u >= 0 && v >= 0 && u + v < 1.01;
+}
+
 function updateHandle(handle: HTMLElement, dx: number, dy: number) {
   const { item, index } = handleMap.get(handle)!;
   const x = item.points[index] + dx;
   const y = item.points[index + 1] + dy;
+
+  const triangle = [...item.points];
+  triangle.splice(index, 2);
+
+  if (isPointInTriangle([x, y], triangle as Triangle)) {
+    return;
+  }
+
   handle.style.left = `${x}px`;
   handle.style.top = `${y}px`;
   item.points[index] = x;
